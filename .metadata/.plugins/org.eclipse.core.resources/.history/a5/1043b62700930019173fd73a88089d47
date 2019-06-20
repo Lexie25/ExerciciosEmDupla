c6a1a713fc34f5a913e38b48c8814599e8a2997b
Brenda <brenda.lima@zup.com.br>
2019-06-19 23:21:31 -0300

    Terminando país

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a5/1043b62700930019173fd73a88089d47
@@ -0,0 +1,14 @@
+package br.com.zup.Países;
+import java.util.ArrayList;
+import java.util.List;
+public class TestePais {
+
+	public static void main(String[] args) {
+
+		List<Pais> listaFronteiras = new ArrayList<Pais>();
+		
+		Pais pais = new Pais ("BRA","Brasil", 8514876);
+		Pais segundoPais = new Pais ("EUA",)
+	}
+
+}
--- Paises/src/br/com/zup/Países/TestePais.java
+++ Paises/src/br/com/zup/Países/TestePais.java
@@ -3,12 +3,46 @@ import java.util.ArrayList;
 import java.util.List;
 public class TestePais {
 
-	public static void main(String[] args) {
+	package br.com.zup.teste;
 
-		List<Pais> listaFronteiras = new ArrayList<Pais>();
-		
-		Pais pais = new Pais ("BRA","Brasil", 8514876);
-		Pais segundoPais = new Pais ("EUA",)
+	import java.util.ArrayList;
+	import java.util.List;
+
+	import br.com.zup.modelo.Pais;
+
+	public class TestePais {
+
+		public static void main(String[] args) {
+			
+			// Lista de países 
+			List<Pais> paisesFronteiras = new ArrayList<Pais>();
+			
+			
+			Pais pais = new Pais("BRA", "Brasil", 8514876);
+			Pais segundoPais = new Pais("EUA", "Estados Unidos", 8514876);
+			Pais terceiroPais = new Pais("FRA", "França", 8514876);
+			Pais quartoPais = new Pais("CAN", "Canada", 8514876);
+			
+			paisesFronteiras.add(segundoPais);
+			paisesFronteiras.add(terceiroPais);
+			paisesFronteiras.add(quartoPais);
+			
+			pais.setpaisesFronteiras(paisesFronteiras);
+			pais.setPopulacao(2000000000);
+			segundoPais.setPopulacao(300000000);
+			
+			// Verificando se os países são iguais
+			System.out.println(pais.verifaSaoIguais(segundoPais));
+			
+			// Verificando densidade populacional
+			System.out.println(pais.calculaDensidade(pais.getPopulacao(), pais.getDimensao()));
+			
+			// Mostrando os países vizinhos
+			System.out.println(pais);
+			
+		}
+
 	}
 
-}
+
+
